f4d9bc84bea5b1471ecc9b959fc45d8c1de59c09
Steven <steven@itheima.com>
2019-04-12 23:07:59 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author Steven
+ * @version 1.0
+ * @description PACKAGE_NAME
+ * @date 2019-4-12
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
